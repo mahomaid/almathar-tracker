0e64fd530f44366589dc39effa05a58135b4f31c
mahomaid <moath.binhomaid@gmail.com>
2026-05-23 14:31:57 +0300

    point to fresh deployment for photo fix

--- config.js
+++ config.js
@@ -2,8 +2,7 @@
 // Both values below are read from this file by the app at startup.
 
 // (1) Apps Script /exec URL
-window.API_URL = 'https://script.google.com/macros/s/AKfycbxP4LwpS7_Rm-ZMwPTkOOld4ojv8biC5frxKaTZhpG16pimD-0_X8MOKsA9u-n3Wsob/exec';
-
+window.API_URL = 'https://script.google.com/macros/s/AKfycby3VXCQ-PKiLcSjcOh97kvpD9vB-ccAJLHz9mh0Vd-miiHt5u_f9-1lCTfGmZRu_Cts/exec';
 // (2) Shared access password
 // Pick something memorable but not obvious. Share verbally / via WhatsApp, not over email.
 // To rotate: change the value here, redeploy, and tell the team the new password.
